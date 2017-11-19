703d5f78d216e56b60685bb256e33c336d31bb2f
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-19 17:50:45 +0000

    Done.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.get ('/', function(req, resp){
+  console.log(req)
+  console.log(resp)
+
+resp.send (`Hello, World!`)
+})
 // Build Your Route Here
 
 
