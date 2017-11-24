281063a920ed473a20d45445e5c9e15ef8a48c61
Kyle Bennett <kyle@bennentterprise.com>
2017-11-24 02:37:34 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get("/", function(req, resp){
+  resp.send("Hello, World!")
+});
 
+app.get("/about", function(req, resp){
+  resp.send("About Page")
+});
 
 module.exports = app
